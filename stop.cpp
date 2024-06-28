5c396d687172a07bc45c14962a118a7b7a98c1d7
deihsi <dvh0xx@outlook.com>
2024-06-28 12:51:32 +0800

    Modify stop.cpp

--- stop.cpp
+++ stop.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Eight send thought.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
