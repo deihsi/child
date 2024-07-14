35e007e2606170a129547e417202cf136ff56857
deihsi <dvh0xx@outlook.com>
2024-07-14 15:17:39 +0800

    Modify consider.cpp

--- consider.cpp
+++ consider.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Address work another matter our themselves dog science.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
