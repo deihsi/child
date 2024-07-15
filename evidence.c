4903005ed7e5b7df4dde80a4c6809b3da9035c2f
deihsi <dvh0xx@outlook.com>
2024-07-15 12:52:57 +0800

    Modify evidence.c

--- evidence.c
+++ evidence.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Arrive whole later interest win inside story.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
