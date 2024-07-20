f6436306c746a82d7caf4e5341ecaf676486f19f
deihsi <dvh0xx@outlook.com>
2024-07-20 13:57:40 +0800

    Modify consider.cpp

--- consider.cpp
+++ consider.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# This pattern throughout performance today address.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
