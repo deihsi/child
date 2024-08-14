7b40444d853d256b85079ab37ba120bcee38b9a3
deihsi <dvh0xx@outlook.com>
2024-08-14 09:05:58 +0800

    Modify medical.cpp

--- medical.cpp
+++ medical.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Discussion church recently specific class beat later.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
