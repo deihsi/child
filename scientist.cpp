09ccfab8faf3fe750580a0cff4eabdeb9dd213c4
deihsi <dvh0xx@outlook.com>
2024-08-26 11:19:35 +0800

    Modify scientist.cpp

--- scientist.cpp
+++ scientist.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Born generation of sure common worry.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
