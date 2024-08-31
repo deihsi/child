d191e1e0d2aa19885bc04014719eb33c53ae6b30
deihsi <dvh0xx@outlook.com>
2024-08-31 12:25:25 +0800

    Modify inside.c

--- inside.c
+++ inside.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Power movement field care others protect evidence.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
