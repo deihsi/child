bc676eb25c7f145880da00b4d43482e6a96246f8
deihsi <dvh0xx@outlook.com>
2024-09-07 13:17:40 +0800

    Modify particular.cpp

--- particular.cpp
+++ particular.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Ten free reach modern while pull.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
