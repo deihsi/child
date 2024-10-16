7f34ad957ffe14fa6fc5c529c847751c48144177
deihsi <dvh0xx@outlook.com>
2024-10-16 11:32:13 +0800

    Modify here.c

--- here.c
+++ here.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Outside official create total alone politics.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
