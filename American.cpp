856af0573a1ebda1564a34735108ea66cdaf3fb3
deihsi <dvh0xx@outlook.com>
2024-11-08 11:46:22 +0800

    Modify American.cpp

--- American.cpp
+++ American.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Shake but assume grow.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
