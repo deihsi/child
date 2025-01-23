2f12f1c995af2200508d12bfc03a679d043f24f9
deihsi <dvh0xx@outlook.com>
2025-01-23 13:30:22 +0800

    Modify small.c

--- small.c
+++ small.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Each adult center sound question hair good change.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
