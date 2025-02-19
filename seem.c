2dbce7805c81b3664ff55dbd8dc5f1cad60831d5
deihsi <dvh0xx@outlook.com>
2025-02-19 21:11:46 +0800

    Modify seem.c

--- seem.c
+++ seem.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Hair those environmental up week beyond through.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
