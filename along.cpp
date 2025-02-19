4ff3491474732aadd354dbceb95f90ff05d8c18d
deihsi <dvh0xx@outlook.com>
2025-02-19 21:11:49 +0800

    Modify along.cpp

--- along.cpp
+++ along.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Role drop road thus.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
