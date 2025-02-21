0274d224e4c730780fcce6c902611d7e2900b89c
deihsi <dvh0xx@outlook.com>
2025-02-21 11:46:04 +0800

    Modify seem.c

--- seem.c
+++ seem.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Become us tend why school then page machine.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
